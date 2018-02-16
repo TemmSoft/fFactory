49e8405428abc3ce06217bf0c8cb1268e2196d5f
TemmSoft <TemmSoft@outlook.com>
2018-02-16 21:19:16 +0300

    maxProfit updates

--- Sources/Algorithms/StockMarket/StockTrends.fs
+++ Sources/Algorithms/StockMarket/StockTrends.fs
@@ -27,17 +27,19 @@ let bestSell lst =
 //    It's not allowed to buy several times before one sell and vice versa. One purchase -> one sell and the same again...
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-let rec f prev min l = match l with 
-                        | h::t when h >= prev -> f h min t
-                        | h::t -> (prev - min) + f h h t
-                        | [] -> 0
-let maxProfitTwo = function h::t -> f h h t | [] -> 0
+let maxProfit1 = function 
+    | head::tail -> let rec f prev min l = match l with 
+                                           | h::t when h >= prev -> f h min t
+                                           | h::t -> (prev - min) + f h h t         // Check tail recursion
+                                           | [] -> 0   
+                    f head head tail 
+    | [] -> 0
 
-[18; 3; 11; 10; 12; 6; 4; 7; 2; 8; 0; 15; 7] |> maxProfitTwo;;
+[0; 18; 3; 11; 10; 12; 6; 4; 7; 2; 8; 0; 15; 7] |> maxProfit1;;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let maxProfitOne lst =
+let maxProfit2 lst =
     let mutable prev = List.head lst
     let mutable min = prev
     [for x in lst do yield (match x with 
@@ -45,11 +47,11 @@ let maxProfitOne lst =
                             | x when x < prev -> let a = prev - min;  
                                                  min <- x; prev <- x; a)] |> List.filter (fun x -> x > 0) |> List.fold (fun acc x -> acc + x) 0
 
-[18; 3; 11; 10; 12; 6; 4; 2; 8; 0; 15; 7] |> maxProfitOne;;
+[18; 3; 11; 10; 12; 6; 4; 2; 8; 0; 15; 7] |> maxProfit2;;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let maxProfitTwo lst =
+let maxProfit3 lst =
     let mutable prev = List.head lst
     let mutable min = prev
     lst |> List.fold (fun acc x -> match x with 
@@ -57,4 +59,4 @@ let maxProfitTwo lst =
                                    | x when x < prev -> let a = prev - min; 
                                                         min <- x; prev <- x; acc + a) 0
 
-[18; 3; 11; 10; 12; 6; 4; 2; 8; 0; 15; 7] |> maxProfitTwo;;
+[18; 3; 11; 10; 12; 6; 4; 2; 8; 0; 15; 7] |> maxProfit3;;

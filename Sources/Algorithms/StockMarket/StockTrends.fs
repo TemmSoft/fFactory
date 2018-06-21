08d06660623169720fa86d482f7b341a7e1976f9
TemmSoft <TemmSoft@outlook.com>
2018-06-21 19:24:03 +0300

    Unit test for maxProfit1 have been added

--- Sources/Algorithms/StockMarket/StockTrends.fs
+++ Sources/Algorithms/StockMarket/StockTrends.fs
@@ -15,10 +15,11 @@ open System
 let maxProfit1 (lst:List<int>) =
     let mutable res = (0,0)
     let mutable min = lst.Head
-    lst |> List.iter (function | x when x < min -> min <- x | x when (x - min) > (snd res) - (fst res) -> res <- (min, x) | _ -> ())
+    lst |> List.iter (function 
+                        | x when x < min -> min <- x 
+                        | x when (x - min) > (snd res) - (fst res) -> res <- (min, x) 
+                        | _ -> ())
     res
-
-[6; 1; 18; 3; 11; 4; 2; 20;8; 0; 15; 7] |> maxProfit1
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -34,8 +35,6 @@ let maxProfit2 = function
                         | [] -> 0   
                     f head head tail 
     | [] -> 0
-
-[17; 18; 3; 11; 10; 10; 10; 12; 6; 4; 7; 2; 2; 2; 8; 0; 15; 7] |> maxProfit2
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -66,13 +65,6 @@ let maxProfit3 verb l =
             | 0 -> printf "It's impossible to find two bargains! \n\n"
             | _ -> printf "maxProfit of two bargain: %i \n\n" (res |> List.max)
 
-
-[18; 2; 6; 9; 7; 3; 11; 10; 12; 6; 19; 8; 9; 5; 17; 4; 13; 8; 6; 11; 7] |> maxProfit3 true
-
-[1] |> maxProfit3 true
-[1;2] |> maxProfit3 true
-[] |> maxProfit3 true
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // TODO: MaxProfit 4: Find the highest possible profit on whole interval with choosed number two of buy-sell tansactions.
@@ -85,74 +77,3 @@ let rec convolute prev min list = match list with
                                   | h::t when min = prev -> convolute h h t
                                   | h::t -> (min, prev)::(convolute h h t)
                                   | [] -> []
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 1    
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-let maxProfit3_One intervals (lst: List<int>) = 
-    let extrems = convolute lst.Head lst.Head lst.Tail
-    let mutable acc = List<int*int>.Empty
-    let mutable matrix = Array2D.create extrems.Length extrems.Length 0
-
-    let updateMtrx max indx =  for i = 0 to indx do matrix.[i,indx] <- snd extrems.[indx] - fst extrems.[i]; 
-                               let max' = matrix.[0..indx,indx] |> Array.max 
-                               if max' > max then max' else max
-
-    let rec aggregate maxIn indx restXtrms = 
-        match restXtrms with       
-             | _::t -> let maxOut = aggregate (updateMtrx maxIn indx) (indx+1) t
-                       let max'' =  matrix.[(indx),(indx)..(Array2D.length2 matrix - 1)] |> Array.max
-                       let res = if max'' > maxOut then max'' else maxOut
-                       if indx > 0 then  acc <- (maxIn,res)::acc else ()
-                       res
-             | [] -> 0
-
-
-    aggregate 0 0 extrems |> ignore
-    let res = acc |> List.map (fun (x,y) -> x + y) |> List.max
-
-    printfn "Input: %A" lst
-    printfn "Extrems: %A" extrems
-    printfn "Matrix:\n %A" matrix
-    printfn "Partials: %A" acc
-    printfn "Result: %A" res
-
-
-let rec convolute1 prev min list = match list with 
-                                  | h::t when h >= prev -> convolute1 h min t
-                                  | h::t when min = prev -> convolute1 h h t
-                                  | h::t -> (min, prev)::(convolute1 h h t)
-                                  | [] -> []
-
-let maxProfit3_Two (lst: List<int>) = 
-    let extrems = convolute1 lst.Head lst.Head lst.Tail
-    let mutable matrix = Array2D.create extrems.Length extrems.Length 0
-
-    let stepForward max indx = for i = 0 to indx do matrix.[i,indx] <- snd extrems.[indx] - fst extrems.[i]; 
-                               let max' = matrix.[0..indx,indx] |> Array.max 
-                               if max' > max then max' else max
-    let stepBackward i max =
-                           let max' =  matrix.[(i),(i)..(Array2D.length2 matrix - 1)] |> Array.max
-                           if max' > max then max' else max
-
-    let aggregate = function
-        | [] -> List<int*int>.Empty
-        | (eMin,eMax)::[] -> [((eMax-eMin),0)]
-        | (eMin,eMax)::t -> 
-                let rec loop indx maxIn acc cont = function//(*
-                                                   | (min,max)::[] -> cont (indx, (max-min), (maxIn,max-min)::acc)
-                                                   | _::t -> let newMax = stepForward maxIn indx
-                                                             t |> loop (indx+1) newMax acc (fun x-> cont ((indx+1), maxIn , acc))
-
-                t |> loop 1 (eMax-eMin) [] stepBackward
-
-    let partials = extrems |> aggregate 
-    let res = partials|> List.map (fun (x,y) -> x + y) |> List.max
-
-    printfn "Input: %A" lst
-    printfn "Extrems: %A" extrems
-    printfn "Matrix:\n %A" matrix
-    printfn "Partials: %A" partials
-    printfn "Result: %A" res
-
-[18; 2; 6; 9; 7; 3; 11; 10; 12; 6; 8; 9; 5; 17; 4; 13; 8; 0; 11; 7] |> maxProfit3_Two
---
+++ UnitTests/XUnitTest/StockMarketTests.fs
@@ -0,0 +1,54 @@
+module StockMarketTests
+
+open System
+open fFactory.StockTrends
+open Xunit
+
+type maxProfit1TestDataProvider () =    
+    let values : seq<obj[]>  = seq {
+                                      yield [| [6; 1; 18; 3; 11; 4; 2; 20; 8; 0; 15; 7]; (1, 20) |]    // 1st test case
+                                      yield [| [1; 7; 18; 3; 11; 4; 2; 20; 8; 3; 15; 0]; (1, 20) |]    // 1st test case
+                                      yield [| [19; 7; 18; 3; 11; 4; 2; 20; 8; 9; 15; 0]; (2, 20) |]    // 1st test case
+                                   }
+    interface seq<obj[]> with
+        member this.GetEnumerator () = values.GetEnumerator()
+        member this.GetEnumerator () = values.GetEnumerator() :> System.Collections.IEnumerator
+
+type maxProfit2TestDataProvider () =    
+    let values : seq<obj[]>  = seq {
+                                      yield [| [6; 1; 18; 3; 11; 4; 2; 20; 8; 0; 15; 7]; 1 |]    // 1st test case
+                                      yield [| [1; 7; 18; 3; 11; 4; 2; 20; 8; 3; 15; 0]; 1 |]    // 1st test case
+                                      yield [| [19; 7; 18; 3; 11; 4; 2; 20; 8; 9; 15; 0]; 2 |]    // 1st test case
+                                   }
+    interface seq<obj[]> with
+        member this.GetEnumerator () = values.GetEnumerator()
+        member this.GetEnumerator () = values.GetEnumerator() :> System.Collections.IEnumerator
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// MaxProfit1 tests
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+[<Theory>]
+[<ClassData(typeof<maxProfit1TestDataProvider>)>]
+let ``MaxProfit 1`` (actual, expected) =
+    Assert.Equal<int * int> (actual |> maxProfit1 , expected)
+ 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// MaxProfit1 tests
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+[<Theory>]
+[<ClassData(typeof<maxProfit2TestDataProvider>)>]
+let ``MaxProfit 2`` (actual, expected) =
+    Assert.Equal<int> (actual |> maxProfit2 , expected)
+ 
+ (*     , 
+[17; 18; 3; 11; 10; 10; 10; 12; 6; 4; 7; 2; 2; 2; 8; 0; 15; 7] |> maxProfit2
+//[<InlineData(6, 1, 18, 3, 11, 4, 2, 20, 8, 0, 15, 7)>]
+[18; 2; 6; 9; 7; 3; 11; 10; 12; 6; 19; 8; 9; 5; 17; 4; 13; 8; 6; 11; 7] |> maxProfit3 true
+
+[1] |> maxProfit3 true
+[1;2] |> maxProfit3 true
+[] |> maxProfit3 true *)

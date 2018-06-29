97540d9acf86572c3a214c0a452a9dee22fb9f46
TemmSoft <TemmSoft@outlook.com>
2018-06-29 17:59:53 +0300

    StockTrends nUnit tests enhancements

--- UnitTests/nUnitTests/StockTrendTests.fs
+++ UnitTests/nUnitTests/StockTrendTests.fs
@@ -1,8 +1,8 @@
 module nUnitTests.StockTrendTests
 
-open fFactory.StockTrends
+open System
 open NUnit.Framework
-
+open fFactory.StockTrends
 
 type maxProfit1TestDataProvider () =    
     let values : seq<obj[]>  = seq {
@@ -30,33 +30,48 @@ type maxProfit2TestDataProvider () =
     
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-// MaxProfit1 tests
+//  MaxProfit1 tests
+//
+//  The tests implemented as a test class and local test data source inside the class.
+//  Test cases are defined with TestCaseSource attribute and local static method as as data source
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 [<TestFixture; Description("Test fixture of MaxProfit 1 function")>] 
 type MaxProfit1Tests() =
-    member x.Actual = [19; 2; 7; 3; 20; 4; 1; 18];
-    member x.Expect = (2, 20)
+    // Source of test data
+    static member testDataSource: seq<obj[]>  = 
+                                    seq {                                       // Test cases...
+                                          yield [| List<int>.Empty; (0, 0) |]
+                                          yield [| [1]; (0, 0) |]
+                                          yield [| [11; 7]; (0, 0) |]
+                                          yield [| [5; 9; 2; 5; 7]; (2, 7) |]          
+                                          yield [| [19; 2; 7; 3; 20; 4; 1; 18]; (2, 20) |]
+                                        }
+    // Tests of iterative version of maxProfit 1                               
+    //  [<Test; Description("Test of Accumulator-based MaxProfit1 function")>] - using TestCaseSource makes using this attribute redundant
+    [<TestCaseSource("testDataSource")>]
+    member x.MaxProfit1AccUnitTest(actual, expected) = Assert.AreEqual(actual |> maxProfit1Iter , expected)
 
-    //[<TestFixtureSetup>]
-    //member x.InitTestFixture() =
-
-
-    [<Test; Description("Accumulator-based function test")>]
-    member x.MaxProfit1AccUnitTest() =
-        Assert.AreEqual(x.Actual |> maxProfit1Acc , x.Expect)
-
-
-    [<Test; Description("Recursive-based function test")>]
-    member x. MaxProfit1RecUnitTest() =
-        Assert.AreEqual(x.Actual |> maxProfit1Rec , x.Expect)
+    // Tests of recursive version of maxProfit 1 
+    [<TestCaseSource("testDataSource")>]
+    member x. MaxProfit1RecUnitTest(actual, expected) = Assert.AreEqual(actual |> maxProfit1Rec , expected)
  
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-// MaxProfit2 tests
+//  MaxProfit2 tests
+//
+//  The tests implemented as a function.
+//  Test cases are defined with TestCaseSource attribute and local static method as as data source
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+type MaxProfit2TestDataSource() =
+    static member testDataSource : seq<obj[]>  = 
+                                    seq {                                                                // Test cases...
+                                            yield [| [6; 1; 18; 3; 0]; 17 |]    
+                                            yield [| [1; 7; 18; 3; 11; 4; 2; 20; 8; 3; 15; 0]; 55 |]   
+                                        }
 //[<Test>]
-//[<TestCaseSource("maxProfit2TestDataProvider")>]
-//let ``MaxProfit 2`` (actual, expected) =
-//    Assert.AreEqual<int> (actual |> maxProfit2 , expected)
+[<TestCaseSource(typeof<MaxProfit2TestDataSource>, "testDataSource")>]
+let ``MaxProfit 2`` actual expected =
+    Assert.AreEqual(actual |> maxProfit2 , expected)
+

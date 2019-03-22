4c1e55b88e24eb2ebf63a0ec8ca3a379a22a8d6f
TemmSoft <TemmSoft@outlook.com>
2019-03-22 17:14:42 +0300

    Adapt code from other solutions

---
+++ Shops/csShop/Lang/Categories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csShop.Lang
+{
+    static public class Categories
+    {
+        static public Func<T1, Func<T2, TRes>> Curry<T1, T2, TRes>(this Func<T1, T2, TRes> func) => a => b => func(a, b);
+        static public Func<T1, Func<T2, Func<T3, TRes>>> Curry<T1, T2, T3, TRes>(this Func<T1, T2, T3, TRes> func) => a => b => c => func(a, b, c);
+        static public Func<T1, Func<T2, Func<T3, Func<T4, TRes>>>> Curry<T1, T2, T3, T4, TRes>(this Func<T1, T2, T3, T4, TRes> func) => a => b => c => d => func(a, b, c, d);
+        static public Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, TRes>>>>> Curry<T1, T2, T3, T4, T5, TRes>(this Func<T1, T2, T3, T4, T5, TRes> func) => a => b => c => d => e => func(a, b, c, d, e);
+        static public Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, TRes>>>>>> Curry<T1, T2, T3, T4, T5, T6, TRes>(this Func<T1, T2, T3, T4, T5, T6, TRes> func) => a => b => c => d => e => f => func(a, b, c, d, e, f);
+        static public Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, TRes>>>>>>> Curry<T1, T2, T3, T4, T5, T6, T7, TRes>(this Func<T1, T2, T3, T4, T5, T6, T7, TRes> func) => a => b => c => d => e => f => g => func(a, b, c, d, e, f, g);
+
+
+    }
+}
---
+++ Shops/csShop/Lang/fpProto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csShop.Lang
+{
+    public class fpProto
+    {
+        static void Proto1()
+        {
+
+
+            Func<int, int, int, int> sammm = (x, y, z) => x + y + z;
+            var aa = sammm.Curry()(1);
+
+
+            ////  var csList = new List<int> { 18, 2, 9, 3, 12, 9, 5, 17, 4, 13, 6, 11, 7 };
+            // var csList = new List<int> { 1, 7, 2, 3, 9 };
+
+            //  var fsList = ListModule.OfSeq<int>(csList); // get FSharpList<int>
+
+            ////  var csRes = CSharpMaxProfits.MaxProfit3_New(csList); 
+            //  var csRes = CSharpMaxProfits.MaxProfit3(csList);
+            //  var fsRes = FSharpMaxProfits.maxProfit3(fsList);
+
+            //  Console.WriteLine($"Max profits: C# - {csRes}, F# - {fsRes}");
+            //  Console.ReadKey();
+        }
+    }
+}
